9a8939c78e4cc391e1071b1bd9e7314f7964abc3
guwoop <morgan.justine@me.com>
2023-11-28 10:03:03 +0100

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://books.toscrape.com/index.html")
+r = requests.get("http://books.toscrape.com/catalogue/1000-places-to-see-before-you-die_1/index.html")
 soup = BeautifulSoup(r.content, "html.parser")
 
 print(soup)
